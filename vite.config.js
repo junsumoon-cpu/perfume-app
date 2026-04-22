689a541607116db2231ca65a51f088ad141bee7b
junsumoon-cpu <junsumoon@omniverse.kr>
2026-04-22 11:49:04 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,7 @@
-import react from '@vitejs/plugin-react-swc';
-import { dirname, resolve } from 'path';
-import { fileURLToPath } from 'url';
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
-
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, './src'),
-    },
-  },
-});
+})
